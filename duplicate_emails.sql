e4cf9391f94e7a73cbf6a6609ba87dee66b9a111
niel8l <niel8l@localhost>
2020-04-29 22:33:33 +0800

    duplicate_emails.sql

---
+++ duplicate_emails.sql
@@ -0,0 +1,20 @@
+-- Write your MySQL query statement below
+
+-- select Email from (
+--     select Email, count(Email) as num
+--     from Person
+--     group by Email
+-- ) as t
+-- where num > 1
+-- ;
+
+-- select Email 
+-- from Person
+-- group by Email
+-- having count(Email) > 1
+-- ;
+
+select distinct p1.Email 
+from Person p1, Person p2
+where p1.Email = p2.Email and p1.id != p2.id
+;
